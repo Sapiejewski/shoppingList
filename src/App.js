80088622e23d1b2157f25c0a831bd849cddf5584
Extriple <patrrykseremak@gmail.com>
2023-07-13 13:37:50 +0200

    polowa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import {useState} from 'react';
+
+// const list=[{name:'apple', quantity:3, bought:false, id:1},
+// {name:'pear', quantity:1, bought:true, id:2},
+// {name:'beef', quantity:8, bought:false, id:3}]
+
+
+
+
+export default App;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Body/>
+      <Footer/>
     </div>
   );
 }
 
-export default App;
+
+
+
+function Body()
+{
+  
+  const [name,setName]=useState("");
+  const [quantity,setQuantity] = useState(1);
+  const[items,setItems] = useState([]);
+  
+  function handleDeleteItem(id){
+    alert("");
+    alert(id);
+    setItems(items=>items.filter(item=>item.id!== id))
+  }
+  return(
+  <>
+    <Form quantity={quantity} name={name} onSetName={setName} onSetQuantity={setQuantity} onSetItems={setItems} items={items}/>
+    <ShoppingList items={items} onHandleDeleteItem={handleDeleteItem}/>
+  </>
+  );
+ }
+
+function Form({items,name,quantity,onSetName,onSetQuantity,onSetItems})
+{
+  function handleAddItems(item){
+    onSetItems(()=>[...items,item])
+  }
+
+
+  function handleSubmit(e)
+  {
+    e.preventDefault();
+    if(!name)  return;
+    const newItem = {name,quantity,bought:false,id:crypto.randomUUID()}
+    handleAddItems(newItem)
+    onSetName("")
+    onSetQuantity(1);
+  }
+
+
+
+return(
+    <form onSubmit={handleSubmit}>
+      <div  className="form">
+      <label>Name</label>
+      <input  value={name} type="text" placeholder="item.." className="input" onChange={(e)=>onSetName(e.target.value)}></input>
+      <label>Quantity</label>
+      <select  value={quantity}className="input" onChange={(e)=>onSetQuantity(Number(e.target.value))}>
+        {Array.from({length:20},(_,i)=>i+1).map(num=><option value={num} key={num}>{num}</option>)}
+      </select>
+      <button>Add item</button>
+      </div>
+    </form>
+);
+}
+
+function ShoppingList({onHandleDeleteItem,items})
+{
+  return(
+  <div className="container">
+    {items.map(item=><Item item={item} onHandleDeleteItem={onHandleDeleteItem} />)}
+  </div>
+  );
+}
+
+function Item({onHandleDeleteItem,item})
+{
+  return(
+    <div className="item">
+      <input type="checkbox" ></input>
+      <span>{item.quantity}</span>
+      <span>{item.name}</span>
+      <span><button onClick={()=>onHandleDeleteItem(item.id)}>❌</button></span>
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import '/home/filip/shoppingList/shopping-list/src/index.css';
+
+export function Header() {
+  return (
+    <header >
+      <h1>Shopping List</h1>
+    </header>
+  );
+}
